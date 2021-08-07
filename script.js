4995098dd1773ebf2291a59bbd3ef3173107722f
vytcka4 <vytautas.freelance@gmail.com>
2021-08-08 00:53:40 +0300

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("form");
+const currentWeight = document.querySelector(".current");
+const goalWeight = document.querySelector(".goal");
+const losingSpeed = document.querySelector(".speed");
+
+form.addEventListener("submit", function (x) {
+  x.preventDefault();
+  current = currentWeight.value;
+  goal = goalWeight.value;
+  speed = losingSpeed.value;
+  let days = 0;
+  let weeks = 0;
+  do {
+    current = current - ((goal / 100) * speed) / 7;
+    days++;
+    if (days === 7) {
+      weeks++;
+      days = days - 7;
+    }
+  } while (current >= goal);
+  console.log(`${weeks} ${days}`);
+});
