bd47d03c9c956a6c48e1758d83420c58a7647e76
vytcka4 <vytautas.freelance@gmail.com>
2021-08-09 02:56:49 +0300

    commit

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const form = document.querySelector("form");
 const currentWeight = document.querySelector(".current");
 const goalWeight = document.querySelector(".goal");
 const losingSpeed = document.querySelector(".speed");
+const result = document.querySelector(".result");
+const resultText = document.querySelector(".result-text");
+let days = 0;
+let weeks = 0;
 
 const calcTime = function () {
   current = currentWeight.value;
   goal = goalWeight.value;
   speed = losingSpeed.value;
-  let days = 0;
-  let weeks = 0;
   do {
     current = current - ((goal / 100) * speed) / 7;
     days++;
@@ -17,10 +19,12 @@ const calcTime = function () {
       days = days - 7;
     }
   } while (current >= goal);
-  console.log(`${weeks} ${days}`);
+  return days, weeks;
 };
 
 form.addEventListener("submit", function (x) {
   x.preventDefault();
   calcTime();
+  result.classList.toggle("hidden");
+  resultText.innerText = `you will need ${weeks} weeks and ${days} days to rech your goal weight of ${goalWeight.value} kg`;
 });
