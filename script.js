44a60f32c51031bb645591e43b52f0ce2f3d03e4
vytcka4 <vytautas.freelance@gmail.com>
2021-08-10 00:52:43 +0300

    commit

--- script.js
+++ script.js
@@ -46,6 +46,20 @@ form.addEventListener("submit", function (x) {
     </tbody>
   `;
     progressTable.insertAdjacentHTML("beforeend", html);
+    if (progress.has(25)) {
+      const html2 = ` <div>
+      <table>
+        <thead class="progress-table">
+          <tr>
+            <th>${key}</th>
+            <th>${value}</th>
+            <th>${currentWeight.value - value}</th>
+          </tr>
+        </thead>
+      </table>
+    </div>`;
+      table.insertAdjacentHTML("beforeend", html2);
+    }
   });
   table.classList.toggle("hidden");
 });
